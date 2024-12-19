009f6611a14aa1ec023f0ee9b51460911a3c00a9
abhinandan-3 <dasariabhnandan@gmail.com>
2024-12-19 23:28:39 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+
+
+
+export default function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Home/>
+    </div>
+  )
+}
